ed9de6ad5234d9eae0fa6c37d11cd5adc05c8084
MacbookHenke <Henrik_junior@hotmail.com>
2023-11-16 15:25:57 +0700

    forms not done(api and validation), did som styling so it looks more like crito design file, issues with new topic menu below header. want same active functions from react router dom as i have in header section.

--- src/views/Contact.jsx
+++ src/views/Contact.jsx
@@ -4,11 +4,26 @@ import Footer from '../components/Footer'
 import ContactForm from '../components/ContactForm'
 import img_mapLocation from '../assets/images/contactPage/map_crito.png'
 import { PrimaryButton } from '../components/general/PrimaryButton'
+import { NavLink } from 'react-router-dom'
 
 const Contact = () => {
   return (
     <>
     <Header/>
+    <div className='contactTopicTop'>
+      <div className='container sectionTitle centerContent'>
+        <div className='wierdTopicMenu'>
+          <div className='topLinks'>
+      <NavLink  className='topicLinks'to="/">
+                Home
+              </NavLink>
+              <NavLink className='topicLinks' to="/Contact">Contact</NavLink>
+              </div>
+        <h2>Let's Connect</h2>
+        </div>
+      </div>
+    
+    </div>
     <div className='newScssPage'>Testar import från seperat scss fil som heter contacts, importen går till index.scss</div>
     <ContactForm/>
     <img className='map' src={img_mapLocation} alt="" />
